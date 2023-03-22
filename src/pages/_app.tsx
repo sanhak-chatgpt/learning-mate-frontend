a1e3632494ca3440c867b3a7670c5707601b9a61
Rengod95 <aorwn970@gmail.com>
2023-03-22 21:46:00 +0900

    feat : set application bootstrap
    * 리액트 쿼리 사용을 위한 부트스트랩 코드를 추가합니다.
    
    * emotion 전역 테마 사용을 위한 부트스트랩 코드를 추가합니다.
    
    * 기본 스타일링 레이아웃을 설정합니다.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Layout } from '@/components/App';
+import { AppContent } from '@/components/App/AppContent';
+import { AppHeader } from '@/components/App/AppHeader';
+import { DEFAULT_THEME } from '@/styles/Theme';
+import '@/styles/globals.css';
+import { ThemeProvider } from '@emotion/react';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import type { AppProps } from 'next/app';
+import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ThemeProvider theme={DEFAULT_THEME}>
+            <Layout>
+              <AppHeader></AppHeader>
+              <AppContent>
+                <Component {...pageProps} />
+              </AppContent>
+            </Layout>
+          </ThemeProvider>
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
+  );
 }
