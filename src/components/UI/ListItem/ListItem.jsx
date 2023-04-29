4916197a35054ee7e1d76e119ff0511b6d8a3594
kjyook <106497893+kjyook@users.noreply.github.com>
2023-04-29 11:41:41 +0900

    4-29 컴포넌트 하나로 통일 , style 코드 조금 수정

--- src/components/UI/ListItem/ListItem.jsx
+++ src/components/UI/ListItem/ListItem.jsx
@@ -6,31 +6,17 @@ export const ListItem = ({ title, description, icon }) => {
 
   const [renderedContent, setRenderedContent] = useState(null);
 
-  if (icon) {
-    setRenderedContent(
-      <S.ListItemWrapperWithIcon>
-        <S.IconStyle>{icon}</S.IconStyle>
-        <S.TitleWithIconStyle>{title}</S.TitleWithIconStyle>
-        <S.DescriptionWithIconStlye>{description}</S.DescriptionWithIconStlye>
-      </S.ListItemWrapperWithIcon>
-    );
-  } else if (description) {
-    setRenderedContent(
-      <S.ListItemWrapper>
-        <S.TitleStyle>{title}</S.TitleStyle>
-        <S.DescriptionStyle>{description}</S.DescriptionStyle>
-      </S.ListItemWrapper>
-    );
-  } else {
-    setRenderedContent(
-      <S.ListItemWrapperOnlyTitle>
-        <S.TitleOnlyStyle>{title}</S.TitleOnlyStyle>
-      </S.ListItemWrapperOnlyTitle>
-    );
-  }
+  setRenderedContent(
+    <S.ListItemWrapper>
+    {icon && <S.IconStyle>{icon}</S.IconStyle>}
+    <div>
+      <S.TitleStyle icon={icon}>{title}</S.TitleStyle>
+      {description && <S.DescriptionStyle>{description}</S.DescriptionStyle>}
+    </div>
+    </S.ListItemWrapper>)
 
-  return <>{renderedContent}</>;
-}
+  return<>{renderedContent}</>;
+};
 
 ListItem.propTypes = {
   title: PropTypes.ReactNode,
--- src/components/UI/ListItem/ListItem.style.jsx
+++ src/components/UI/ListItem/ListItem.style.jsx
@@ -3,29 +3,10 @@ import styled from '@emotion/styled';
 //그냥 listitem
 export const ListItemWrapper = styled.div`
     position: relative;
-    width: 331px;
+    width: x;
     height: 82px;
-    box-sizing: border-box;
     border-bottom: 1px solid #F1F3F8;
-    background-color: #FFFFFF;
-`;
-
-//icon 있는 listitem
-export const ListItemWrapperWithIcon = styled.div`
-    position: relative;
-    width: 374px;
-    height: 86px;  
-    background: #FFFFFF;
-`;
-
-//title 만 있는 listitem
-export const ListItemWrapperOnlyTitle = styled.div`
-    box-sizing: border-box;
-    position: relative;
-    width: 331px;
-    height: 72px;
-    background: #FFFFFF;
-    border-bottom: 1px solid #F1F3F8;
+    background: ${({ theme }) => theme.color.background.default};
 `;
 
 //title과 description만 있는 경우
@@ -39,42 +20,10 @@ export const TitleStyle = styled.div`
     font-family: 'Pretendard';
     font-style: normal;
     font-weight: 600;
-    font-size: 15px;
-    line-height: 18px;
-
-    color: #4E5966;
-`;
-
-//title과 icon, description이 있는 경우
-export const TitleWithIconStyle = styled.div`
-    position: absolute;
-    left: 21.39%;
-    right: 33.69%;
-    top: 26.74%;
-    bottom: 52.33%;
-
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 700;
     font-size: ${({ theme }) => theme.size.font.md};
     line-height: 18px;
-    color: ${({ theme }) => theme.color.text.g0};
-`;
 
-//title 만 있는 경우
-export const TitleOnlyStyle = styled.div`
-    position: absolute;
-    left: 0%;
-    right: 72.51%;
-    top: 37.5%;
-    bottom: 37.5%;
-
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 15px;
-    line-height: 18px;
-    color: #4E5966;
+    color: ${({ theme }) => theme.color.text.g2};
 `;
 
 export const IconStyle = styled.div`
@@ -83,23 +32,6 @@ export const IconStyle = styled.div`
     right: 81.28%;
     top: 20.93%;
     bottom: 20.93%;
-`;
-
-//icon이 있는 description
-export const DescriptionWithIconStlye = styled.div`
-    position: absolute;
-    left: 21.39%;
-    right: 24.33%;
-    top: 54.65%;
-    bottom: 26.74%;
-
-    font-family: 'Pretendard';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 13px;
-    line-height: 16px;
-
-    color: ${({ theme }) => theme.color.text.g3};
 `;
 
 //그냥 description
@@ -113,7 +45,7 @@ export const DescriptionStyle = styled.div`
     font-family: 'Pretendard';
     font-style: normal;
     font-weight: 400;
-    font-size: 13px;
+    font-size: ${({ theme }) => theme.size.font.xs};
     line-height: 16px;
 
     color: ${({ theme }) => theme.color.text.g3};
