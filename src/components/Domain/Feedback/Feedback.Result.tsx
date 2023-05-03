35dd9be58f68f4deeb838afe1d5ff871f6acc6c8
kjyook <106497893+kjyook@users.noreply.github.com>
2023-05-04 02:20:25 +0900

    style : feedback result 페이지 divder로 간격 맞추기

--- src/components/Domain/Feedback/Feedback.Result.tsx
+++ src/components/Domain/Feedback/Feedback.Result.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import * as S from './FeedbackForm.styles';
 import { SVGIcon } from '@/components/UI/SVGIcon';
 import { useFeedbackRecordContoller } from '@/components/Domain/Feedback/FeedbackRecord.hooks';
@@ -16,15 +17,24 @@ export type ResultProps = {
     result: LectureDtoResponse | undefined;
 };
 
+const Divider = styled.div`
+  width: 100%;
+  height: 16px;
+  background: ${({ theme }) => theme.color.background.default};
+`;
+
 const FeedbackResult = ({result}: ResultProps) => {
     return (
         <S.Root>
             <S.Wrapper flex={'columnStart'}>
                 <S.ResultContainer flex={'columnStart'}>
-                    <ListItem title='나의 강의' description={'나도 몰라'}></ListItem>
-                    <ListItem title='점수' description={result?.score + '/100'} ></ListItem>
-                    <ListItem title='좋았던 점' description={result?.strength}></ListItem>
-                    <ListItem title='개선하면 좋은 점' description={result?.weakness}></ListItem>
+                    <ListItem title='나의 강의' description={'나도 몰라'} />
+                    <Divider />
+                    <ListItem title='점수' description={result?.score + '/100'} />
+                    <Divider />
+                    <ListItem title='좋았던 점' description={result?.strength} />
+                    <Divider />
+                    <ListItem title='개선하면 좋은 점' description={result?.weakness} />
                 </S.ResultContainer>
             </S.Wrapper>
         </S.Root>
