c6b65dd3981068f9a25970c9881af10091d740ac
Rengod95 <aorwn970@gmail.com>
2023-05-16 02:29:37 +0900

    feat : implement AudioMediaRecorder.ts
    하나의 훅이 리액트의 렌더링 사이클 + 미디어 레코더 configuration + 미디어 레코더와 렌더링 사이클 연동 + 과정에서 발생하는 사이드 이펙트 관리 + 미디어 레코더의 이벤트 핸들러 제어
    
    의 많은 책임을 담당하던 코드를 분리하여 클래스화 합니다.
    
    해당 클래스는 Audio 를 입력받고, 해당 데이터를 관리하는 로직이 작성됩니다.
    
    생성자를 통해 미디어 레코더의 configuration을 주입받습니다.
    
    해당 클래스가 생성되는 시점과 실제 미디어 레코더가 생성되는 시점을 분리합니다. 이렇게 함으로써, 이벤트 핸들러가 등록되는 시점을 미디어 레코더 인스턴스가 생성되는 직후로 제어할 수 있어 이벤트 핸들러가 등록되는 과정에서 발생하는 제어할 수 없는 오류들을 최대한 방지합니다.
    즉 미디어 레코더가 생성되는 시점을 별도의 메소드로 통제합니다. 이는 마이크 권한의 유무에 따른 미디어 레코더 생성 통제를 조금 더 유연하게 할 수 있습니다.
    
    훅으로 미디어 레코드를 관리할 경우, 무조건 미디어 레코더의 생성시점에서 이벤트 핸들러가 주입되어야 하는 것을 분리하여, 단일 책임의 원칙을 지킬 수 있습니다.
    
    추후 브릿지 코드를 통해 인앱 마이크 권한을 얻는 함수를 작성해야 합니다.

---
+++ src/util/AudioRecorder.ts
@@ -0,0 +1,110 @@
+import { getDeviceEnvironment } from '@/util/Agent';
+import { BaseError } from '@/util/Error';
+
+export interface RecorderConfig {
+  onStart?: (...args: any) => void;
+  onDataAvailable?: (...args: any) => void;
+  onStop?: (...args: any) => void;
+  onResume?: (...args: any) => void;
+  onPause?: (...args: any) => void;
+  onError?: (...args: any) => void;
+}
+
+export type NativePermissionState = 'granted' | 'denied' | 'permanently_denied';
+
+export const createNativeAudioMediaRecorder = (status: NativePermissionState) => {
+  if (status === 'granted') {
+    // return new AudioMediaRecorder();
+  }
+  return undefined;
+};
+
+export const getNativeMicrophonePermission = () => {
+  window.FlutterBridge?.postMessage('requestMicrophonePermission');
+};
+
+export class AudioMediaRecorder {
+  recorder: MediaRecorder | undefined;
+  private totalRecordBlob: Blob | undefined;
+  private audioChunks: Blob[] = [];
+
+  constructor(private recorderConfig: RecorderConfig) {}
+
+  public startRecord = () => {
+    this.recorder?.start();
+  };
+
+  public stopRecord = () => {
+    this.recorder?.stop();
+  };
+
+  public resumeRecord = () => {
+    this.recorder?.resume();
+  };
+
+  public pauseRecord = () => {
+    this.recorder?.pause();
+  };
+  public getRecorderConfig = () => {
+    return this.recorderConfig;
+  };
+
+  public initAudioRecorder = async () => {
+    if (!this.recorder) {
+      this.recorder = await this.createMediaRecorder();
+      this.setRecorderConfig();
+    }
+  };
+
+  private setRecorderConfig = () => {
+    if (!!this.recorder) {
+      console.log('record config set');
+      this.recorder.onstart = () => {
+        this.recorderConfig?.onStart?.();
+      };
+
+      this.recorder.onstop = () => {
+        this.saveTotalRecordBlob();
+        this.recorderConfig?.onStop?.();
+        this.recorder = undefined;
+      };
+
+      this.recorder.ondataavailable = (e) => {
+        this.audioChunks.push(e.data);
+        this.recorderConfig?.onDataAvailable?.();
+      };
+      this.recorder.onpause = () => {
+        this.recorderConfig?.onPause?.();
+      };
+
+      this.recorder.onresume = () => {
+        this.recorderConfig?.onResume?.();
+      };
+
+      this.recorder.onerror = () => {
+        this.recorderConfig?.onError?.();
+      };
+    }
+  };
+
+  public getRecorderState = () => {
+    return this.recorder?.state;
+  };
+
+  public getTotalRecordBlob = () => {
+    return this.totalRecordBlob;
+  };
+
+  private saveTotalRecordBlob = () => {
+    this.totalRecordBlob = new Blob(this.audioChunks, { type: 'audio/wav' });
+  };
+
+  private createMediaRecorder = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      return new MediaRecorder(stream);
+    } catch (e) {
+      throw new BaseError('마이크 권한을 허용해주세요!');
+    }
+  };
+}
