a0c9d5ac9828ef4c4e0eea917bb3887bd882505f
Rengod95 <aorwn970@gmail.com>
2023-05-16 02:18:11 +0900

    feat : implement ObservableTimer class
    리액트의 렌더링 주기와 쉽게 동기화할 수 있는 Observable 한 Timer 클래스를 구현합니다.
    
    Timer의 인터벌 마다 notify를 실행하여, 옵저버 함수들을 콜백으로 실행합니다.
    
    이때 옵저버함수가 타이머 객체 인스턴스에 있는 멤버와 함수를 참조할 수 있도록, this를 바인딩하여 구현합니다.
    
    이를 통해, 객체 인스턴스에 직접적으로 접근하지 못하는 훅과 같은 외부 컨텍스트에서 손쉽게 타이머 인스턴스를 제어하거나 내부의 데이터를 참조하는 옵저버 함수를 등록할 수 있습니다.

---
+++ src/util/Timer.ts
@@ -0,0 +1,54 @@
+import { Observable, ObserverFunc } from '@/util/Observable';
+
+export type TimerObserver = ObserverFunc<ObservableTimer>;
+
+export class ObservableTimer implements Observable<ObservableTimer, TimerObserver> {
+  private time = 0;
+  private timer: NodeJS.Timer | undefined;
+  observers: TimerObserver[] = [];
+  constructor() {}
+
+  public startTimer = () => {
+    this.timer = setInterval(() => {
+      this.time += 1;
+      this.notify();
+    }, 1000);
+  };
+
+  public stopTimer = () => {
+    clearInterval(this.timer);
+  };
+
+  public resetTimer = () => {
+    this.stopTimer();
+    this.time = 0;
+    this.notify();
+  };
+
+  public subscribe = (obs: TimerObserver, ...args: unknown[]) => {
+    const boundedObserver = obs.bind(this, ...args);
+    this.observers.push(boundedObserver);
+
+    //unsubscribe callback, 외부에서 인라인 함수로 unsubscribe를 하지 못하기 때문에 이에 대해 대응
+    return () => {
+      const targetIndex = this.observers.indexOf(boundedObserver);
+      this.observers = this.observers.filter((obs, idx) => idx !== targetIndex);
+    };
+  };
+
+  public notify = () => {
+    this.observers.forEach((obs) => {
+      obs.call(this);
+    });
+  };
+
+  public getFormattedTime = () => {
+    const minutes = Math.floor(this.time / 60);
+    const remainingSeconds = this.time % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+  };
+
+  public getTimeBySecond = () => {
+    return this.time;
+  };
+}
