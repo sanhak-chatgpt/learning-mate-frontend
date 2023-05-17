4e430443a26badf3e9b2d8b8daa3a3d3711d5718
Rengod95 <aorwn970@gmail.com>
2023-05-17 20:31:27 +0900

    feat : implement AgentController class
    클라이언트가 동작하는 환경에 대한 정보를 관리하는 AgentController 클래스를 구현합니다.
    
    해당 클래스를 통해, 웹뷰 환경과 같은 다양한 환경에 대한 정보를 얻을 수 있도록 구현할 예정입니다.
    
    따로 분리되어있던 getDeviceEnvironment 함수를 해당 클래스에 종속시켰습니다.

--- src/util/Agent.ts
+++ src/util/Agent.ts
@@ -1,25 +1,25 @@
-declare global {
-  interface Window {
-    FlutterBridge?: {
-      postMessage: (status: string) => void;
-    };
-  }
-}
-
 export type DeviceEnvironment = 'PC' | 'MOBILE' | 'WEBVIEW';
 
-export const getDeviceEnvironment = (): DeviceEnvironment => {
-  const userAgent = navigator.userAgent;
+export class AgentController {
+  private getDeviceEnvironment = (): DeviceEnvironment => {
+    const userAgent = navigator.userAgent;
 
-  // on webview
-  if (window.FlutterBridge) {
-    return 'WEBVIEW';
-  }
-  // on Mobile Browser
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
-    return 'MOBILE';
-  }
+    // on webview
+    if (window.FlutterBridge) {
+      return 'WEBVIEW';
+    }
+    // on Mobile Browser
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
+      return 'MOBILE';
+    }
 
-  // On PC Browser
-  return 'PC';
-};
+    // On PC Browser
+    return 'PC';
+  };
+
+  public isOnWebview = () => {
+    return this.getDeviceEnvironment() === 'WEBVIEW';
+  };
+}
+
+export const agentController = new AgentController();
