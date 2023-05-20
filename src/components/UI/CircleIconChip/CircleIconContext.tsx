28c993d9fd5cb3183202f8a902c9ecbb8dcd4074
Rengod95 <aorwn970@gmail.com>
2023-05-20 17:28:11 +0900

    chore : remove svg assets

--- src/components/UI/CircleIconChip/CircleIconContext.tsx
+++ src/components/UI/CircleIconChip/CircleIconContext.tsx
@@ -1,9 +1,9 @@
-import React, { Dispatch, ReactNode, SetStateAction } from "react";
-import { createContext } from "react";
+import React from 'react';
+import { createContext } from 'react';
 
 type CircleIconContextProps = {
-    isChecked: (value?: string) => boolean;
-    toggleValue: (value?: string) => void;
+  isChecked: (value?: string) => boolean;
+  toggleValue: (value?: string) => void;
 };
 
 export const CircleIconContext = createContext<CircleIconContextProps | null>(null);
