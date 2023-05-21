6d96e140268a2cc5d6155f55056dc2f13fb6b03a
kjyook <106497893+kjyook@users.noreply.github.com>
2023-05-21 15:59:01 +0900

    feat: ranking header

---
+++ src/components/App/AppHeader/HeaderContent/HeaderContentImpls/RankingHeader.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { userState } from '@/states/state.user';
+
+export const RankingHeader = () => {
+  return (
+    <>
+      <h1>
+        랭킹페이지
+      </h1>
+    </>
+  );
+};
+
+export const RankingDescription = () => {
+  const user = useRecoilValue(userState);
+
+  return (
+    <>
+      <h2>
+        {user.name}님 러닝메이트에서 학습하신 기록으로
+        <br />
+        {user.name}님의 학습점수를 확인하실 수 있어요!
+      </h2>
+    </>
+  );
+};
--- src/components/App/AppHeader/HeaderContent/HeaderContentImpls/index.ts
+++ src/components/App/AppHeader/HeaderContent/HeaderContentImpls/index.ts
@@ -1,3 +1,4 @@
 export * from './404Header';
 export * from './MainHeader';
 export * from './FeedbackHeader';
+export * from './RankingHeader';
