8f17b46428cf89e6c3c1170a63dff715106d0e04
kjyook <106497893+kjyook@users.noreply.github.com>
2023-05-21 15:33:01 +0900

    feat: ranking page 구현, icon registry 등록

---
+++ src/components/Domain/Ranking/Ranking.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from './Ranking.styles'
+import { SVGIcon } from "@/components/UI/SVGIcon";
+
+export const Ranking = () => {
+    return(
+        <SVGIcon name={'RankingPageIcon'} width={250} height={250} viewBox={'0 0 250 250'}></SVGIcon>
+
+    )
+};
--- src/components/UI/SVGIcon/SVGIcon.registry.ts
+++ src/components/UI/SVGIcon/SVGIcon.registry.ts
@@ -14,6 +14,7 @@ export const SVGIconRegistry = {
   faceNormal: () => import('./templates/FaceNormal'),
   faceGood: () => import('./templates/FaceGood'),
   faceBest: () => import('./templates/FaceBest'),
+  RankingPageIcon: () => import('./templates/RankingPageIcon')
 };
 
 export type IconRegistryKey = keyof typeof SVGIconRegistry;
