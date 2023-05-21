f099b5037990c962ac69675c4b9b7f03167f9a9f
Rengod95 <aorwn970@gmail.com>
2023-05-21 15:18:09 +0900

    fix : 타입 미선언으로 인한 빌드에러 수정

--- src/components/UI/Theme/ThemeProvider.tsx
+++ src/components/UI/Theme/ThemeProvider.tsx
@@ -5,8 +5,7 @@ import { localStorageManager } from "@/util/models/Storage";
 import { useEffect } from "react";
 import { useControllTheme } from "./ThemeProvider.hooks";
 
-export type ThemeProviderProps =  React.PropsWithChildren<{
-}>
+export type ThemeProviderProps =  React.PropsWithChildren<any>
 
 
 export const ThemeProvider = ({children}:ThemeProviderProps) => {
