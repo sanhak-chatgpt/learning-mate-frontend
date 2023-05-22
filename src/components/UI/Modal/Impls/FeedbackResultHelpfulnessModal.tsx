220ca83aa70224778c44f810ec1fac13fc052b5f
kjyook <106497893+kjyook@users.noreply.github.com>
2023-05-22 20:28:48 +0900

    refactor: map함수를 이용하여 함수 간단하게

--- src/components/UI/Modal/Impls/FeedbackResultHelpfulnessModal.tsx
+++ src/components/UI/Modal/Impls/FeedbackResultHelpfulnessModal.tsx
@@ -4,10 +4,25 @@ import { DefaultModalProps } from '@/components/UI/Modal/Modal.types';
 import CircleIconChip from '@/components/UI/CircleIconChip/CircleIconChip';
 import { IconContainer } from '@/components/UI/Modal/Impls/FeedbackResultHelpfulnessModal.styles';
 import { CircleIconGroup } from '@/components/UI/CircleIconChip/CircleIconGroup';
+import { IconRegistryKey, ViewBoxSize } from '../../SVGIcon';
 
 export type FeedbackHelpModalProps = {
   title: string;
 } & DefaultModalProps;
+
+export type IconItem<TName = IconRegistryKey, TBox = ViewBoxSize> = {
+  name: TName;
+  width: string | number;
+  height: string | number;
+  viewBox: TBox;
+}
+export const CIRCLE_ICON_PRESET: Array<IconItem> = [
+  { name:'faceWorst' , width:20 , height:20 , viewBox:'0 0 20 20' },
+  { name:'faceBad' , width:24 , height:24 , viewBox:'0 0 24 24' },
+  { name:'faceNormal' , width:24 , height:24 , viewBox:'0 0 24 24' },
+  { name:'faceGood' , width:24 , height:24 , viewBox:'0 0 24 24' },
+  { name:'faceBest' , width:20 , height:20 , viewBox:'0 0 20 20' },
+]
 
 const FeedbackResultHelpfulnessModal = ({ title, visible, close }: FeedbackHelpModalProps) => {
   const [iconName, setIconName] = useState<string>("");
@@ -19,6 +34,11 @@ const FeedbackResultHelpfulnessModal = ({ title, visible, close }: FeedbackHelpM
           <header>{title}</header>
           <IconContainer as={'main'} flex={'rowCenter'} >
             <CircleIconGroup values={iconName} onChange={setIconName} >
+              {/* {CIRCLE_ICON_PRESET?.map((icon) => {
+                return (
+                  <CircleIconChip name={icon.name} width={icon.width} height={icon.height} viewBox={icon.viewBox} />
+                );
+              })} */}
               <CircleIconChip name={'faceWorst'} width={20} height={20} viewBox={'0 0 20 20'}  />
               <CircleIconChip name={'faceBad'} width={24} height={24} viewBox={'0 0 24 24'}  />
               <CircleIconChip name={'faceNormal'} width={24} height={24} viewBox={'0 0 24 24'}  />
