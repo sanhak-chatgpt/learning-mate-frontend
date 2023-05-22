0ca75bd63fa2cf9c60a2e1919f47a43bec16af1d
kjyook <106497893+kjyook@users.noreply.github.com>
2023-05-22 15:49:38 +0900

    refator: ranking header hook 분리

--- src/components/Domain/Ranking/Ranking.tsx
+++ src/components/Domain/Ranking/Ranking.tsx
@@ -1,31 +1,10 @@
 import React, { useCallback, useEffect } from "react";
 import * as S from './Ranking.styles'
 import { SVGIcon } from "@/components/UI/SVGIcon";
-import {
-    RankingHeader,
-    RankingDescription,
-  } from '@/components/App/AppHeader/HeaderContent/HeaderContentImpls/RankingHeader';
-import { useRecoilState } from 'recoil';
-import { headerContentState } from '@/states/state.header';
+import { useSettingHeader } from "./Ranking.hooks";
 
 export const Ranking = () => {
-    const [headerContent, setHeaderContent] = useRecoilState(headerContentState);
-    
-    const initializeHeaderContent = useCallback(() => {
-        setHeaderContent({
-            title: RankingHeader,
-            description: RankingDescription,
-            backward: {
-                visible: false,
-                historyStack: [],
-            },
-        });
-    }, []);
-
-    useEffect(() => {
-        initializeHeaderContent();
-    }, []);
-
+    useSettingHeader();
       
     return(
         <SVGIcon name={'RankingPageIcon'} width={250} height={250} viewBox={'0 0 210 210'}></SVGIcon>
